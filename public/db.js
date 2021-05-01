5247492b74e8398b4c99b8874e77803ad6f39fa4
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-05-01 00:00:38 -0400

    added db.js to store data offline

---
+++ public/db.js
@@ -0,0 +1,64 @@
+//creating db for offline use
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore("pending", {
+        keyPath: "id",
+        autoIncrement: true
+    });
+};
+
+request.onsuccess = (event) => {
+    db = event.target.result;
+
+    // check if app online
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+//catch error
+request.onerror = (event) => {
+    console.log("Page error " + event.target.errorCode);
+  };
+
+  //if fetch fails, saveRecord will be called to save trasnsaction 
+function saveRecord(record) {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction("pending", "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+  }
+
+  //function called when app goes back online
+function checkDatabase() {
+    const transaction = db.transaction("pending", "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+     //post all pending transactions stored in indexedDB to online database
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        // if successful, empty stored transactions from indexedDB
+        const transaction = db.transaction("pending", "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+}
+
+//listen for app coming back online
+window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++
@@ -1,1 +0,0 @@
-console.log("Hi from your service-worker.js file!")
